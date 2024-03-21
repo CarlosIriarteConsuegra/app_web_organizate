a30c3bb11aaae9415ee61f4d23e122a21d28faa4
carlos.iriarte <proyamerika@gmail.com>
2024-03-21 16:51:04 -0500

    refresh de token

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,25 +1,52 @@
+import { catchError, concatMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor, HTTP_INTERCEPTORS
+  HttpInterceptor, HTTP_INTERCEPTORS, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { TokenService } from '../services/seguridad/token.service';
+import { AuthService } from '../services/seguridad/auth.service';
+import { TokenDto } from '../models/seguridad/token.dto';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService) { }
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (!this.tokenService.isLogged()) {
+      return next.handle(request);
+    }
     let intReq = request;
     const token = this.tokenService.getToken();
-    if (token !== null) {
-      intReq = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
-    }
-    return next.handle(intReq);
+    intReq = this.addToken(request, token);
+    return next.handle(intReq).pipe(catchError((err: HttpErrorResponse) => {
+      if (err.status === 401) {
+        const dto: TokenDto = new TokenDto();
+        return this.authService.postRefresh(dto).pipe(concatMap((data: any) => {
+          console.log('refreshing...');
+          this.tokenService.setToken(data.token);
+          intReq = this.addToken(request, data.token);
+          return next.handle(intReq);
+        }));
+      } else {
+        this.tokenService.deslogger();
+        this.router.navigate(['/auth/login']);
+        return throwError(err);
+      }
+    }));
+  }
+
+  private addToken(request: HttpRequest<unknown>, token: string | null): HttpRequest<unknown> {
+    return request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
   }
 }
 
--- src/app/services/seguridad/auth.service.ts
+++ src/app/services/seguridad/auth.service.ts
@@ -5,6 +5,7 @@ import { ConfigUrlService } from '../../../core/configUrl-service';
 import { BaseService } from '../base.service';
 import { UsuariosDTO } from '../../models/seguridad/usuarios.dto';
 import { environment } from '../../../enviroments/environment';
+import { TokenDto } from '../../models/seguridad/token.dto';
 
 @Injectable()
 export class AuthService extends BaseService {
@@ -16,6 +17,10 @@ export class AuthService extends BaseService {
 
   postLogin(usuario: UsuariosDTO) {
     return this.http.post<any>(`${environment.microproxy_seguridad}auth/login`, usuario);
+  }
+
+  postRefresh(token: TokenDto) {
+    return this.http.post<any>(`${environment.microproxy_seguridad}auth/refresh`, token);
   }
 
 }
